9029e7211726c91ebd4e52f2156036fa72a0ea43
waillyn <waillyn2013@gmail.com>
2026-03-16 12:47:13 -0400

    Agregar CRUD completo
    Agregar CRUD completo productos, almacenes e inventario

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,22 @@
+from flask import Flask
+from config import Config
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Registrar Blueprint principal
+    from app.routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+
+
+
+
+""""
 from flask import Flask,render_template
 from config import Config
 
@@ -30,3 +49,4 @@ def create_app(config_class=Config):
     app.register_blueprint(main_bp)
 
     return app
+"""
